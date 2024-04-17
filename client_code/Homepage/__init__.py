592bc3fa872d40675d0aa2feddc3398fdc56e084
loganbcarter@msn.com <loganbcarter@msn.com>
2024-04-17 11:00:42 +0000

    Created Individual_InitialSelection and Homepage; Edited settings; Deleted Form1

---
+++ client_code/Homepage/__init__.py
@@ -0,0 +1,42 @@
+from ._anvil_designer import HomepageTemplate
+from anvil import *
+
+class Homepage(HomepageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def is_button_select_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    global variables
+    variables= {}
+    self.homepage_buttons.visible = False
+    save_clicked = alert(
+      content = Individual_InitialSelection(item=variables),
+      title = "Select Variables For Individual",
+      large=True,
+      buttons=[("Save", True), ("Cancel", False)],
+    )
+    if save_clicked:
+        self.individualhome_card.visible = True
+        app_tables.selectedvariables_i.delete_all_rows()
+        app_tables.selectedvariables_i.add_row(initialrank=variables['initialrank'],
+                                            sex=variables['sex'],
+                                            age=variables['age'],
+                                            maritalstatus=variables['maritalstatus'],
+                                            employment=variables['employment'],
+                                            education=variables['education'],
+                                            country=variables['country'],
+                                            adjustedrank=variables['adjustedrank'],
+                                            name=variables['name'])
+        self.name_label.text = variables['name']
+        self.initialrank_label.text = variables['initialrank']
+        self.adjustedrank_label.text = variables['adjustedrank']
+        self.age_label.text = variables['age']
+        self.sex_label.text = variables['sex']
+        self.maritalstatus_label.text = variables['maritalstatus']
+        self.employment_label.text = variables['employment']
+        self.education_label.text = variables['education']
+        self.country_label.text = variables['country'] 
--- client_code/Individual_InitialSelection/__init__.py
+++ client_code/Individual_InitialSelection/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Individual_InitialSelectionTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class Individual_InitialSelection(Individual_InitialSelectionTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
