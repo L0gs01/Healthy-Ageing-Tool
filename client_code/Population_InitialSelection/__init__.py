5e34cd7808fdfe3028784913691285860927c906
loganbcarter@msn.com <loganbcarter@msn.com>
2024-05-21 18:05:34 +0000

    Edited Population_InitialSelection, Population_Repeating, Population_Repeating.ItemTemplate1, Slider and 1 more; Deleted Population_Repeating.ItemTemplate2 and Population_Repeating.ItemTemplate3

--- client_code/Population_InitialSelection/__init__.py
+++ client_code/Population_InitialSelection/__init__.py
@@ -13,107 +13,32 @@ class Population_InitialSelection(Population_InitialSelectionTemplate):
     global pop_selection
     # Any code you write here will run before the form opens.
 
-  # def slider_1_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_1.text = self.slider_1.level
-
-  # def slider_2_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_2.text = self.slider_2.level
-
-  # def slider_3_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_3.text = self.slider_3.level
-
-  # def slider_4_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_4.text = self.slider_4.level
-
-  # def slider_5_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_5.text = self.slider_5.level
-
-  def textbox_6_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    self.label_6.text = self.textbox_6.text
-    
-  def slider_7_change(self, **event_args):
-    """This method is called when the slider is moved"""
-    self.label_7.text = self.slider_7.level
-   
-  def slider_8_change(self, **event_args):
-    """This method is called when the slider is moved"""
-    self.label_8.text = self.slider_8.level
-
-  def slider_9_change(self, **event_args):
-    """This method is called when the slider is moved"""
-    self.label_9.text = self.slider_9.level
-
-  def dropdown_10_change(self, **event_args):
-    """This method is called when an item is selected"""
-    self.label_10.text = self.dropdown_10.selected_value
-
-  # def slider_11_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_11.text = self.slider_11.level
-
-  # def slider_12_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_12.text = self.slider_12.level
-
-  # def slider_13_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_13.text = self.slider_13.level
-
-  # def slider_13_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_13.text = self.slider_13.level
-
-  # def slider_14_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_14.text = self.slider_14.level
-
-  # def slider_15_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_15.text = self.slider_15.level
-
-  # def slider_16_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_16.text = self.slider_16.level
-
-  # def slider_17_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_17.text = self.slider_17.level
-
-  # def slider_18_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_18.text = self.slider_18.level
-
-  # def slider_19_change(self, **event_args):
-  #   """This method is called when the slider is moved"""
-  #   self.label_19.text = self.slider_19.level
-
-  # def slider_20_change(self, **event_args):
+  # def slider__change(self, **event_args):
   #   """This method is called when the slider is moved"""
   #   self.label_20.text = self.slider_20.level
 
+  def percent_slider_change(self, level, **event_args):
+    """This method is called when the slider is moved"""
+    self.percent_label.text = self.percent_slider.level
 
-  def save_btn_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    global textbox_6_end
-    textbox_6_end = (int(self.textbox_6.text)*4)
-    global pop_selection
-    pop_selection = {self.textbox_6.text,
-                     self.slider_7.level,
-                     self.slider_8.level,
-                     self.slider_9.level,
-                     self.dropdown_10.selected_value}
+  # def save_btn_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   global textbox_6_end
+  #   textbox_6_end = (int(self.textbox_6.text)*4)
+  #   global pop_selection
+  #   pop_selection = {self.textbox_6.text,
+  #                    self.slider_7.level,
+  #                    self.slider_8.level,
+  #                    self.slider_9.level,
+  #                    self.dropdown_10.selected_value}
     
-  def show_btn_click(self, **event_args):
-    """This method is called when the button is clicked"""
+  # def show_btn_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
     
-    print(pop_selection)
-    print(textbox_6_end)
+  #   print(pop_selection)
+  #   print(textbox_6_end)
+
+  
 
 
 
