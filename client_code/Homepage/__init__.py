0c67fab7ba634ff5123c4a5b5b62bc5e065d4a9f
loganbcarter@msn.com <loganbcarter@msn.com>
2024-05-22 14:52:29 +0000

    Edited Homepage and Population_InitialSelection

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -214,12 +214,16 @@ class Homepage(HomepageTemplate):
       print(pop_variables)
       self.populationhome_card.visible = True
       #app_tables.pop_info.delete_all_rows()
-      app_tables.pop_info.add_row(pop_country_dd=pop_variables['country'],
-                                 pop_age_dd=pop_variables['age'],
-                                 pop_initial_dd=pop_variables['initial'],
-                                 pop_percent_slider=pop_variables['percent'],
-                                 pop_percentsuccess_slider=pop_variables['percent_s'],
-                                 pop_adjusted_dd=pop_variables['adjusted'])
+      app_tables.pop_info.add_row(pop_country_dd=str(pop_variables['country']),
+                                 pop_age_dd=str(pop_variables['age']),
+                                 pop_initial_dd=str(pop_variables['initial']),
+                                 pop_percent_slider=str(pop_variables['percent']),
+                                 pop_percentsuccess_slider=str(pop_variables['percent_s']),
+                                 pop_adjusted_dd=str(pop_variables['adjusted']))
+      percent_impact = (1000*(int(pop_variables['percent'])/100))
+      print(percent_impact)
+      percent_success = (percent_impact*(int(pop_variables['percent_s'])/100))
+      print(percent_success)
 
 
 
