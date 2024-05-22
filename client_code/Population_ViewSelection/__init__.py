d3fb6984f066fb136f36b913f2fc9b43560f6ed4
loganbcarter@msn.com <loganbcarter@msn.com>
2024-05-22 15:56:15 +0000

    Edited settings, Population_ViewSelection.ItemTemplate1 and Population_ViewSelection

--- client_code/Population_ViewSelection/__init__.py
+++ client_code/Population_ViewSelection/__init__.py
@@ -4,11 +4,15 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil.designer import in_designer
+
 
 
 class Population_ViewSelection(Population_ViewSelectionTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.pop_info.search()
+
     # Any code you write here will run before the form opens.
+    self.pop_repeating_panel.items = app_tables.pop_info.search()
+
